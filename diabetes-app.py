5d0cc5fdfd0f92895e39fd08b40aa59041b69cfc
DenyWisnuSS <109987304+DenyWisnuSS@users.noreply.github.com>
2024-01-28 02:28:49 +0700

    Add files via upload

---
+++ diabetes-app.py
@@ -0,0 +1,62 @@
+import pickle
+import streamlit as st
+import numpy as np
+
+# Membaca model
+diabetes_model = pickle.load(open('ensemble_model.sav', 'rb'))
+
+# Judul web
+st.title('Sistem Prediksi Penyakit Diabetes')
+
+# Deskripsi web
+st.write("""
+Aplikasi ini berbasis Machine Learning dengan menggunakan model ensemble yang telah dilatih pada kumpulan data diabetes. Aplikasi ini dapat memprediksi apakah seseorang terkena diabetes atau tidak, berdasarkan nilai **Glucose, BMI, dan Usia**.
+
+**Cara menggunakan aplikasi:**
+
+1. Masukkan nilai Glucose, BMI, dan usia Anda.
+2. Klik tombol "Prediksi".
+3. Aplikasi akan menampilkan hasil prediksi, apakah Anda terkena diabetes atau tidak.
+""")
+# Input untuk nilai-nilai variabel
+# Membagi kolom
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    Glucose = st.number_input('Input nilai Glucose (mg/dL)', value=None, key='Glucose')
+
+with col2:
+    BMI = st.number_input('Input nilai BMI (kg/m^2)', value=None, key='BMI')
+
+with col3:
+    Age = st.number_input('Input nilai Usia/Umur (tahun)', value=None, key='Age', step=1)
+
+# Code untuk prediksi
+diabetes_diagnosis = ''
+
+# Tombol untuk prediksi
+if st.button('Prediksi'):
+    try:
+        # Mengonversi input ke tipe numerik
+        data_input = np.array([[Glucose, BMI, int(Age)]])
+
+        # Prediksi menggunakan model
+        diabetes_prediction = diabetes_model.predict(data_input)
+
+        # Menampilkan hasil prediksi
+        if diabetes_prediction[0] == 0:
+            diabetes_diagnosis = 'Pasien Tidak Terkena Diabetes'
+            color = 'green'
+        else:
+            diabetes_diagnosis = 'Pasien Terkena Diabetes'
+            color = 'red'
+
+        # Menampilkan prediksi dengan warna latar belakang yang sesuai dan border yang lebih berjarak
+        st.markdown(
+            f'<div style="background-color:{color}; padding: 10px; border-radius: 5px; margin-bottom: 10px;"><b>{diabetes_diagnosis}</b></div>',
+            unsafe_allow_html=True)
+
+    except ValueError:
+        st.error('Pastikan semua input adalah angka.')
+    except Exception as e:
+        st.error(f'Terjadi kesalahan: {e}')
